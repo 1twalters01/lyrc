59e05347e5fa7f8fdc299f5992ab0a4557d9821e
Tyler Walters <1twalters01@gmail.com>
2026-07-22 11:29:13 +0100

    fix pyo3 root in script

--- scripts/tests/pyo3.sh
+++ scripts/tests/pyo3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 
 export VIRTUAL_ENV="$ROOT/.venv"
 export PYTHONHOME="$($VIRTUAL_ENV/bin/python -c 'import sys; print(sys.base_prefix)')"
