df8d4f380cb54ca0040fb0faf6dd6e9898662f14
Tyler Walters <1twalters01@gmail.com>
2026-07-25 02:41:56 +0100

    rearchitectuing how the app will run

--- crates/core/src/renderer.rs
+++ crates/core/src/renderer.rs
@@ -1,7 +1,9 @@
 use crate::state::AppState;
 
 pub trait Renderer {
+    type Target;
     type Error;
 
+    // fn render(&mut self, target: Self::Target, state: &AppState) -> Result<(), Self::Error>;
     fn render(&mut self, state: &AppState) -> Result<(), Self::Error>;
 }
