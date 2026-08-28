788beaa55050c22f3f20570d1a24d9f26cac071b
Tyler Walters <1twalters01@gmail.com>
2026-08-29 00:43:41 +0100

    initial word synchronizer

--- crates/synchronizer/src/strategies/words.rs
+++ crates/synchronizer/src/strategies/words.rs
@@ -1,10 +1,10 @@
 use std::usize;
 
 use chrono::Duration;
-use subtitles::subtitles::SubtitleDocument;
+use subtitles::subtitles::{SubtitleContent, SubtitleDocument};
 
 use crate::{
-    strategies::lyrics::CueIndex,
+    strategies::lyrics::{CueIndex, LyricsSynchronizer},
     traits::{CueIndexed, Synchronizer},
 };
 
@@ -46,10 +46,76 @@ impl Synchronizer for WordSynchronizer {
         subtitle_document: &Option<SubtitleDocument>,
         position: &Option<Duration>,
     ) -> Option<Self::Event> {
+        let (subtitle_document, position) = match (subtitle_document, position) {
+            (Some(subtitle_document), Some(position)) => (subtitle_document, position),
+            (_, _) => return None,
+        };
+
+        let new_words = Self::get_words_at(&subtitle_document, Some(position));
+
+        if new_words != self.active_words {
+            let old_words = std::mem::replace(&mut self.active_words, new_words);
+
+            let event = WordSyncEvent::Changed {
+                old_words,
+                new_words: self.active_words.clone(),
+            };
+
+            return Some(event);
+        }
+
         None
     }
 
     fn get_active_indices(&self) -> &[WordIndex] {
         &self.active_words
+    }
+}
+
+impl WordSynchronizer {
+    pub fn new() -> Self {
+        Self {
+            active_words: Vec::new(),
+        }
+    }
+
+    pub fn get_words_at(
+        subtitle_document: &SubtitleDocument,
+        position: Option<&Duration>,
+    ) -> Vec<WordIndex> {
+        let position = match position {
+            Some(position) => position,
+            None => return Vec::new(),
+        };
+
+        let current_cue_indices =
+            LyricsSynchronizer::get_cues_at(subtitle_document, Some(position));
+
+        current_cue_indices
+            .iter()
+            .flat_map(|cue_index| {
+                let cue = &subtitle_document.cues[cue_index.cue];
+
+                match &cue.content {
+                    SubtitleContent::Text(_) => Vec::new().into_iter(),
+
+                    SubtitleContent::Words(words) => words
+                        .iter()
+                        .enumerate()
+                        .filter_map(|(word_index, word)| {
+                            if word.start <= *position && *position < word.end {
+                                Some(WordIndex {
+                                    cue: cue_index.cue,
+                                    word: word_index,
+                                })
+                            } else {
+                                None
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                        .into_iter(),
+                }
+            })
+            .collect()
     }
 }
